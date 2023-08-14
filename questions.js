aa8b8c9f5470be5ca43dc8bfce86b1f247a426b5
Geovany7 <gustavogeovany154@gmail.com>
2023-08-13 21:29:28 -0300

    zorp

---
+++ questions.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      question: "Qual dos seguintes tipos de software é utilizado para criar e editar documentos de texto?",
+      answers: [
+        { option: "a) Planilha", correct: false },
+        { option: "b) Antivírus", correct: false },
+        { option: "c) Navegador", correct: false },
+        { option: "d) Processador de Texto", correct: true },
+      ],
+    },
+    {
+      question: 'O que é um "sistema operacional" em termos de software?',
+      answers: [
+        { option: "a) Um programa de edição de vídeo", correct: false },
+        { option: "b) Um conjunto de ferramentas de design gráfico", correct: false },
+        { option: "c) Um software para gerenciar hardware e software em um computador", correct: true },
+        { option: "d) Um navegador de internet", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o termo usado para um software que ajuda a proteger um sistema ou rede ao monitorar e controlar o tráfego de rede?",
+      answers: [
+        { option: "a) Firewall", correct: true },
+        { option: "b) Processador de Texto", correct: false },
+        { option: "c) Software de Design Gráfico", correct: false },
+        { option: "d) Planilha", correct: false },
+      ],
+    },
+    {
+      question: "Em qual geração de computadores os sistemas operacionais começaram a ser desenvolvidos, permitindo a interação mais amigável com os computadores?",
+      answers: [
+        { option: "a) Segunda geração", correct: false },
+        { option: "b) Terceira geração", correct: true },
+        { option: "c) Quarta geração", correct: false },
+        { option: "d) Quinta geração", correct: false },
+      ],
+    },
+    {
+      question: 'Qual geração de computadores é marcada pela introdução dos primeiros microcomputadores ou "computadores pessoais"?',
+      answers: [     
+        { option: "a) Segunda geração", correct: false },
+        { option: "b) Terceira geração", correct: false },
+        { option: "c) Quarta geração", correct: true },
+        { option: "d) Quinta geração", correct: false },
+      ],
+    },
+    {
+      question: "Em qual geração de computadores houve avanços significativos na capacidade de armazenamento e processamento, com a introdução dos discos rígidos e microprocessadores mais avançados?",
+      answers: [     
+        { option: "a) Segunda geração", correct: false },
+        { option: "b) Terceira geração", correct: true },
+        { option: "c) Quarta geração", correct: false },
+        { option: "d) Quinta geração", correct: false },
+      ],
+    },
+      {
+        question: 'O que é um "malware"?',
+        answers: [
+          { option: "a) Um programa de edição de imagens", correct: false },
+          { option: "b) Um software de backup", correct: false },
+          { option: "c) Um tipo de hardware de computador", correct: false },
+          { option: "d) Um software malicioso projetado para prejudicar, invadir ou roubar informações de um sistema", correct: true },
+        ],
+      },
+      {
+        question: "Qual é o termo usado para programas maliciosos que fingem ser legítimos, mas na verdade têm a intenção de roubar informações pessoais ou causar danos ao sistema?",
+        answers: [
+          { option: "a) Spyware", correct: true },
+          { option: "b) Firewall", correct: false },
+          { option: "c) Backup", correct: false },
+          { option: "d) Navegador", correct: false },
+        ],
+      },
+      {
+        question: "Qual é a finalidade de um software antivírus?",
+        answers: [
+          { option: "a) Acelerar a conexão com a internet", correct: false },
+          { option: "b) Criptografar arquivos pessoais", correct: false },
+          { option: "c) Proteger contra malware e vírus", correct: true },
+          { option: "d) Fornecer acesso a sites restritos", correct: false },
+        ],
+      },
+      {
+        question: 'O que é um "sistema de gerenciamento de relacionamento com o cliente" (CRM)?',
+        answers: [
+          { option: "a) Um programa de edição de texto", correct: false },
+          { option: "b) Um software de backup", correct: false },
+          { option: "c) Um sistema para rastrear interações e relacionamentos com clientes", correct: true },
+          { option: "d) Um aplicativo de mensagens", correct: false },
+        ],
+      },
+  ];
